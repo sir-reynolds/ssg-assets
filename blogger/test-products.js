e14d067b0377b4e53d8a911f41e250948f36fe99
Caprice J. Reynolds <37714879+sir-reynolds@users.noreply.github.com>
2020-12-23 21:33:29 +0700

    Update test-products.js

--- blogger/test-products.js
+++ blogger/test-products.js
@@ -1,3 +1,5 @@
+/** random number **/
+function randNum(max){x= Math.random().toString();x=x.substring(19 - max, 19);return x;};
 
 /** schema jsonld **/
 (function(){
@@ -12,15 +14,15 @@ var Cat2Uri=window.location.origin+"/search/label/"+Math.floor((Math.random() * 
 	
   if ( schema_recipe == true ) {
   	
-  	var schema = [ { "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [ { "@type": "ListItem", "position": 1, "item": { "@id": Cat1Uri, "name": _SCHCAT1_name } }, { "@type": "ListItem", "position": 2, "item": { "@id": Cat2Uri, "name": _SCHCAT2_name } } ] }, { "@context": "https://schema.org", "@type": "Recipe", "author": window.location.hostname, "name": Params['title'], "datePublished": new Date().toISOString().slice(0, 10), "description": Params['title'], "image": "https://i2.wp.com/ae01.alicdn.com/kf/"+Params['img'], "interactionStatistic": { "@type": "InteractionCounter", "interactionType": "https://schema.org/Comment", "userInteractionCount": randNum(4) }, "aggregateRating": { "@type": "AggregateRating", "ratingValue": "5", "ratingCount": randNum(5) } } ];
+  	var schema = [ { "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [ { "@type": "ListItem", "position": 1, "item": { "@id": Cat1Uri, "name": Breadcrumb1 } }, { "@type": "ListItem", "position": 2, "item": { "@id": Cat2Uri, "name": Breadcrumb2 } } ] }, { "@context": "https://schema.org", "@type": "Recipe", "author": window.location.hostname, "name": document.title, "datePublished": new Date().toISOString().slice(0, 10), "description": document.title, "image": "https://i2.wp.com/ae01.alicdn.com/kf/"+Params['img'], "interactionStatistic": { "@type": "InteractionCounter", "interactionType": "https://schema.org/Comment", "userInteractionCount": randNum(4) }, "aggregateRating": { "@type": "AggregateRating", "ratingValue": "5", "ratingCount": randNum(5) } } ];
   
   } else if ( schema_products == true ) {
   	
-  	var schema = [ { "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [ { "@type": "ListItem", "position": 1, "item": { "@id": Cat1Uri, "name": _SCHCAT1_name } }, { "@type": "ListItem", "position": 2, "item": { "@id": Cat2Uri, "name": _SCHCAT2_name } } ] }, { "@context": "https://schema.org", "@type": "Product", "aggregateRating": { "@type": "AggregateRating", "ratingValue": "4", "reviewCount": randNum(6) }, "description": Params['title'], "name": Params['title'], "image": "https://i2.wp.com/ae01.alicdn.com/kf/"+Params['img'] } ];
+  	var schema = [ { "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [ { "@type": "ListItem", "position": 1, "item": { "@id": Cat1Uri, "name": Breadcrumb1 } }, { "@type": "ListItem", "position": 2, "item": { "@id": Cat2Uri, "name": Breadcrumb2 } } ] }, { "@context": "https://schema.org", "@type": "Product", "aggregateRating": { "@type": "AggregateRating", "ratingValue": "4", "reviewCount": randNum(6) }, "description": document.title, "name": document.title, "image": "https://i2.wp.com/ae01.alicdn.com/kf/"+Params['img'] } ];
   
   } else {
     
-    var schema = [ { "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [ { "@type": "ListItem", "position": 1, "item": { "@id": Cat1Uri, "name": _SCHCAT1_name } }, { "@type": "ListItem", "position": 2, "item": { "@id": Cat2Uri, "name": _SCHCAT2_name } } ] } ];
+    var schema = [ { "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [ { "@type": "ListItem", "position": 1, "item": { "@id": Cat1Uri, "name": Breadcrumb1 } }, { "@type": "ListItem", "position": 2, "item": { "@id": Cat2Uri, "name": Breadcrumb2 } } ] } ];
   
   };
   
