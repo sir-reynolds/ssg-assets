fe6db24b105caa1f05c995b8779acefb320aaf25
Caprice J. Reynolds <37714879+sir-reynolds@users.noreply.github.com>
2020-12-23 16:30:15 +0700

    Update and rename test-archive5.js to test-archive6.js

--- blogger/test-archive6.js
+++ blogger/test-archive6.js
@@ -7,7 +7,7 @@ function callback_archive(data) {
     			  productImage	=	data[i].imageUrl;
     			  productId	=	data[i].productId;
     			  productTitle	=	data[i].productTitle;
-    			  productPrice	=	data[i].productTitle;
+    			  productPrice	=	data[i].salePrice;
     			  permalink	=	data[i].blogger_permalink;
                 
     			  inhtml += loop_tpl_view
@@ -28,12 +28,24 @@ function callback_archive(data) {
     	};
 };
 
+
 (function(){
-    	if ( typeof currentPage != 'undefined' && typeof pagination_tpl_id != 'undefined' && typeof pagination_tpl_view != 'undefined' ) {
+	/** insert pagination **/
+	if ( typeof currentPage != 'undefined' && typeof pagination_tpl_id != 'undefined' && typeof pagination_tpl_view != 'undefined' ) {
 		if ( currentPage != '' && pagination_tpl_id != '' && pagination_tpl_view != '' ) {
-			var prevU=(prevN=(currentPage - 1)) < 2 ? "/" : "/search/label/"+prevN; nextU=(nextN=(currentPage + 1)) < 403974 ? "/search/label/"+nextN : "/"; pagitTpl=pagination_tpl_view.replace("{prev_url}",prevU).replace("{next_url}",nextU); document.getElementById(pagination_tpl_id).innerHTML = pagitTpl;
+			var prevU=(prevN=(currentPage - 1)) < 2 ? "/" : "/search/label/"+prevN; 
+			var nextU=(nextN=(currentPage + 1)) < 403974 ? "/search/label/"+nextN : "/"; 
+			var pagitTpl=pagination_tpl_view.replace("{prev_url}",prevU).replace("{next_url}",nextU); 
+			document.getElementById(pagination_tpl_id).innerHTML = pagitTpl;
 		}
 	}
+	
+	/** call product api **/
+	var e=document.createElement("script");
+	e.type="text/javascript";
+	e.src="https://jsonp-bot-cacheno.staticapis.cyou/AEX/22des2020-js-paging?page="+currentPage;
+	(document.body||document.getElementsByTagName('body')[0]).appendChild(e);
 })();
 
-(function(){var e=document.createElement("script");e.type="text/javascript";e.src="https://jsonp-bot-cacheno.staticapis.cyou/AEX/22des2020-js-paging?page="+currentPage;(document.body||document.getElementsByTagName('body')[0]).appendChild(e);})();
+
+
